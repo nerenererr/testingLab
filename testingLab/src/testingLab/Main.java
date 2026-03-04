f556b0fb6b95573d258e1cab2057bccc777db295
nerenererr <dinrss22@gmail>
2026-03-04 14:10:12 +0100

    "first commit"

---
+++ testingLab/src/testingLab/Main.java
@@ -0,0 +1,9 @@
+package testingLab;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ testingLab/test/testingLab/MiddleEarthBattleTest.java
@@ -0,0 +1,13 @@
+package testingLab;
+import org.junit.jupiter.api.Test;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MiddleEarthBattleTest {
+	
+	MiddleEarthBattle battle = new MiddleEarthBattle();
+	
+	@Test
+	
+
+}
